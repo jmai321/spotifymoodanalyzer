9b0c7003086f0ed62a401b8eb1914e7cff8abcd0
Kendrick Ngo <kendrickngo200)@github.com>
2023-10-27 05:29:52 -0700

    styled login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles.css'
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function LoginPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <head>
+        <link rel="stylesheet" href="./styles.css" />
+      </head>
+      <div className="LoginPage">
+        <div id="login-container">
+          <div className="login">
+            <h1>Login</h1>
+            <button id="login-button" className="big-btn">Log in with Spotify</button>
+            <p className="login-desc">Connect your Spotify Account.</p>
+            <p className="login-desc-small">You will automatically be redirected to this page after login.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+// export default App;
+export default LoginPage;
